20b03789cd1cf6a4aa51aad4a4805f009c6d696f
Ievgen Kuzminov <kyzminov@gmail.com>
2016-04-26 15:43:49 +0300

    add toc

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ var main = {
     
     // show the big header image	
     main.initImgs();
+    main.initToc();
   },
   
   initImgs : function() {
@@ -103,6 +104,27 @@ var main = {
 	} else {
 	  $(".img-desc").hide();  
 	}
+  },
+
+  initToc : function(){
+        $('#toc').toc({
+        'selectors': 'h1,h2,h3,h4,h5,h6', //elements to use as headings
+        'container': '.posts-list', //element to find all selectors in
+        'smoothScrolling': true, //enable or disable smooth scrolling on click
+        'prefix': 'toc', //prefix for anchor tags and class names
+        'onHighlight': function(el) {}, //called when a new section is highlighted 
+        'highlightOnScroll': true, //add class to heading that is currently in focus
+        'highlightOffset': 100, //offset to trigger the next headline
+        'anchorName': function(i, heading, prefix) { //custom function for anchor name
+            return $(heading).data('toc');
+        },
+        'headerText': function(i, heading, $heading) { //custom function building the header-item text
+            return $heading.text();
+        },
+    'itemClass': function(i, heading, $heading, prefix) { // custom function for item class
+      return $heading[0].tagName.toLowerCase();
+    }
+    });
   }
 };
 
